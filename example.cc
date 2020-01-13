5b4e6b9fe40db35afd472fc8ed743d5c4c4ae86e
Peter Salvi <vukung@yahoo.com>
2020-01-13 17:00:01 +0100

    Test program bugfix

--- example.cc
+++ example.cc
@@ -5,34 +5,32 @@
 
 using namespace Geometry;
 
-std::vector<PointVector> readGBP(std::string filename) {
+std::vector<PointVector> readBezierBoundary(std::string filename) {
   std::ifstream f(filename.c_str());
   f.exceptions(std::ios::failbit | std::ios::badbit);
 
   size_t n, d;
   f >> n >> d;
-  size_t l = (d + 1) / 2;
-  size_t cp = 1 + d / 2;
-  cp = n * cp * l + 1;          // # of control points
 
   Point3D p;
-  f >> p[0] >> p[1] >> p[2];    // central control point
+  f >> p[0] >> p[1] >> p[2];    // central control point (not used)
 
   std::vector<PointVector> result;
   PointVector ribbon;
-  for (size_t i = 1, side = 0, col = 0; i < cp; ++i, ++col) {
+  for (size_t col = 0, side = 0; true; ++col) {
     if (col > d) {
-      col = 0;
+      col = 1;
       result.push_back(ribbon);
       auto last = ribbon.back();
       ribbon.clear();
       ribbon.push_back(last);
-      if (++side >= n)
+      if (++side == n)
         break;
     }
     f >> p[0] >> p[1] >> p[2];
     ribbon.push_back(p);
   }
+  result.back().back() = result.front().front();
 
   return result;
 }
@@ -47,7 +45,7 @@ int main(int argc, char **argv) {
   if (argc == 3)
     resolution = std::atoi(argv[2]);
 
-  auto curves = readGBP(argv[1]);
+  auto curves = readBezierBoundary(argv[1]);
   std::vector<C0Coons::RationalCurve> boundaries;
   std::transform(curves.begin(), curves.end(), std::back_inserter(boundaries),
                  [&](const PointVector &points) {
